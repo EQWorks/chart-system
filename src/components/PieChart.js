deaf8c6290c4bc2ba3fccb71a080369e9338539c
Tamires Lowande <tamires.lowande@gmail.com>
2020-03-12 09:55:24 -0400

    highlight the arc being hovered and gray the others

--- src/components/PieChart.js
+++ src/components/PieChart.js
@@ -1,12 +1,13 @@
-import React from 'react'
-
-// import PropTypes from 'prop-types'
+import React, { useState } from 'react'
+import PropTypes from 'prop-types'
 import { ResponsivePie } from '@nivo/pie'
 // import numeral from 'numeral'
 
 
 
-// const propTypes = {
+const propTypes = {
+  width: PropTypes.number,
+  height: PropTypes.number
 //   data: PropTypes.arrayOf(PropTypes.object).isRequired,
 //   selectedDataType: PropTypes.string.isRequired,
 //   fieldConfig: PropTypes.arrayOf(PropTypes.shape({
@@ -14,9 +15,10 @@ import { ResponsivePie } from '@nivo/pie'
 //     name: PropTypes.string.isRequired,
 //     format: PropTypes.string,
 //   })).isRequired,
-// }
+}
 
-const PieChart = ({ width, data, selectedDataType, fieldConfig }) => {
+// export default function PieChart ({ width='100%', height='300px', data, selectedDataType, fieldConfig }) {
+export default function PieChart ({ width='100%', height='300px' }) {
   // make sure parent container have a defined height when using
 // responsive component, otherwise height will be 0 and
 // no chart will be rendered.
@@ -24,137 +26,169 @@ const PieChart = ({ width, data, selectedDataType, fieldConfig }) => {
 // you'll often use just a few of them.
 
 
-//   const formattedData = formatData(data, selectedDataType)
-//   console.log(formattedData)
+  //   const formattedData = formatData(data, selectedDataType)
+  //   console.log(formattedData)
 
-//FUTURE DATA FROM LOCUS - uncomment lines 31-57
-// let d = {
-//     "Totals": {
-//         "visits": 7782,
-//         "visitors": 3525,
-//         "repeat_visitors": 1695,
-//         "single_visitors": 3243,
-//         "multi_visitors": 282,
-//         "repeat_visitor_rate": 0.460430967943783,
-//         "multi_visitor_rate": 0.0792857567675623
-//  }
-// }
-  
-// const parseData = (obj) => {
-//     let data = obj.Totals
-//     let finalData = []
-//     for (let visit in data){
-//         let element = {
-//             id: visit,
-//             label: visit.length > 10 ? `${visit.slice(0,10)}...` : visit,
-//             value: data[visit]
-//         }
-//         finalData.push(element)
-//     }
-//     return finalData
-// }
+  //FUTURE DATA FROM LOCUS - uncomment lines 31-57
+  // let d = {
+  //     "Totals": {
+  //         "visits": 7782,
+  //         "visitors": 3525,
+  //         "repeat_visitors": 1695,
+  //         "single_visitors": 3243,
+  //         "multi_visitors": 282,
+  //         "repeat_visitor_rate": 0.460430967943783,
+  //         "multi_visitor_rate": 0.0792857567675623
+  //  }
+  // }
 
-// const formattedData = parseData(d)
+  // const parseData = (obj) => {
+  //     let data = obj.Totals
+  //     let finalData = []
+  //     for (let visit in data){
+  //         let element = {
+  //             id: visit,
+  //             label: visit.length > 10 ? `${visit.slice(0,10)}...` : visit,
+  //             value: data[visit]
+  //         }
+  //         finalData.push(element)
+  //     }
+  //     return finalData
+  // }
 
-//DATA FROM RANDOM EXAMPLE
-const formattedData = [
+  // const formattedData = parseData(d)
+
+  //--------------------------------------
+  //DATA FROM RANDOM EXAMPLE
+  const formattedData = [
     {
-      "id": "erlang",
-      "label": "erlang",
-      "value": 461,
-      "color": "#0039ac"
+      'id': 'erlang',
+      'label': 'erlang',
+      'value': 461,
+      // 'color': '#0039ac'
     },
     {
-      "id": "elixir",
-      "label": "elixir",
-      "value": 300,
-      "color": "#0084ff"
+      'id': 'elixir',
+      'label': 'elixir',
+      'value': 300,
+      // 'color': '#0084ff'
     },
     {
-      "id": "css",
-      "label": "css",
-      "value": 66,
-      "color": "#e6f0ff"
+      'id': 'css',
+      'label': 'css',
+      'value': 66,
+      // 'color': '#e6f0ff'
     },
     {
-      "id": "scala",
-      "label": "scala",
-      "value": 135,
-      "color": "#9ac4fb"
+      'id': 'scala',
+      'label': 'scala',
+      'value': 135,
+      // 'color': '#9ac4fb'
     },
     {
-      "id": "stylus",
-      "label": "stylus",
-      "value": 387,
-      "color": "#0062d9"
+      'id': 'stylus',
+      'label': 'stylus',
+      'value': 387,
+      // 'color': '#0062d9'
     },
   ]
 
-  
- function percentData(){
+  //--------------------------------------
+  function percentData(){
     let sum = 0
 
     for (let d of formattedData){
-        sum = sum + d.value
-    } 
+      sum = sum + d.value
+    }
     formattedData.forEach(arc => {
-        arc.percent = `${(arc.value * 100 /sum).toFixed(2)}%`
+      arc.percent = `${(arc.value * 100 /sum).toFixed(2)}%`
     })
     return sum
   }
+  //--------------------------------------
   percentData()
-//   console.log(formattedData)
-
+  //   console.log(formattedData)
+  //--------------------------------------
   const style = {
-      height: 300,
-      width: width,
-      backgroundColor: 'lightGrey'
+    height: height,
+    width: width,
+    backgroundColor: 'lightGrey'
   }
-
+  //--------------------------------------
   const legend = {
     legends: [
-        {
-          anchor: 'right',
-          direction: 'column',
-          translateY: 0,
-          translateX: 20,
-          itemWidth: 0,
-          itemHeight: 22,
-          itemTextColor: '#999',
-          symbolSize: 18,
-          symbolShape: 'circle',
-          effects: [
-            {
-              on: 'hover',
+      {
+        anchor: 'right',
+        direction: 'column',
+        translateY: 0,
+        translateX: 20,
+        itemWidth: 0,
+        itemHeight: 22,
+        itemTextColor: '#999',
+        symbolSize: 18,
+        symbolShape: 'circle',
+        effects: [
+          {
+            on: 'hover',
             //   style: { itemTextColor: 'color' },
-              style: { itemTextColor: '#000' },
-            },
-          ],
-        }
-      ]
+            style: { itemTextColor: '#000' },
+          },
+        ],
+      }
+    ]
   }
-  
-  
-  const arcLabel = e => 
-     (<>
+
+  //--------------------------------------
+  const arcLabel = e =>
+    (<>
       <tspan x="0" y="0">{e.percent}</tspan>
       <tspan x="0" y="15">{e.label} </tspan>
-      </>
-     )
+    </>
+    )
+  //--------------------------------------
+  const designSystemColors = ['#0039ac', '#0084ff', '#e6f0ff', '#9ac4fb','#0062d9']
 
+  const [colors, setColors] = useState(designSystemColors)
 
+  //--------------------------------------
+  const mouseLeaveHandler = ( ) => {
+    setColors(designSystemColors)
+
+  }
+
+  const mouseOverHandler = (_data, event) => {
+    console.log('enter pie')
+    let arc = event.target
+
+    //  ( event.target !== arc ? mouseLeaveHandler() : null)
+
+    let newColors = [...colors].map(color => {
+      return color !== arc.getAttribute('fill') ?
+        color = 'lightgray'
+        : color
+    })
+
+    setColors(newColors)
+
+    // event.target.style.fill = 'black'
+    // event.target.style.fill = '#9EE1CE';
+    // event.target.style.cursor = 'pointer'
+    // event.target.style.strokeWidth = '2'
+    // event.target.style.opacity = '0.2'
+  }
+  //--------------------------------------
   return (
     <div style={style}>
       <ResponsivePie
         data={formattedData}
-        margin={{ top: 36, right: 36, bottom: 36, left:36}}
-        {...style.width >= 400 && {margin: {top: 36, right: 120, bottom: 36, left:36}}}
+        margin={{ top: 36, right: 36, bottom: 36, left: 36}}
+        {...style.width >= 400 && {margin: {top: 36, right: 120, bottom: 36, left: 36}}}
         sortByValue
         padAngle={0.7}
         cornerRadius={3}
         //---------------
         //FROM https://github.com/plouc/nivo/issues/477
-        colors={['#0039ac', '#0084ff', '#e6f0ff', '#9ac4fb','#0062d9']}
+        colors={colors}
         // colors={{ scheme: 'blues' }}
         // colors={d => d.color}
         //---------------
@@ -171,17 +205,17 @@ const formattedData = [
         // radialLabelsLinkStrokeWidth={1}
         // radialLabelsLinkColor={{ from: 'color' }}
         {...style.width <= 300 ? {sliceLabel: arcLabel} : {sliceLabel: 'percent'} }
-        {...style.height <= 300 ? {slicesLabelsSkipAngle:20} : {slicesLabelsSkipAngle:10}}
+        {...style.height <= 300 ? {slicesLabelsSkipAngle: 20} : {slicesLabelsSkipAngle: 10}}
         slicesLabelsTextColor='white'
         // slicesLabelsTextColor='#333333'
         animate
         motionStiffness={90}
         motionDamping={15}
-        tooltip={(e) => 
-            <>
+        tooltip={(e) =>
+          <>
             <span> {e.id}: </span>
             <strong>{e.value} </strong>
-            </>
+          </>
         }
 
         {...style.width >= 400 && legend }
@@ -196,35 +230,39 @@ const formattedData = [
         // }}
 
         //FROM https://github.com/plouc/nivo/issues/295 & https://github.com/plouc/nivo/issues/724
-        onMouseEnter={(_data, event) => {
-          console.log(_data)
-          console.log(event.target)
-          // formattedData.forEach(d => {
-            // console.log(d.color)
-            // if (d.color !== _data.color) {
-              //fill all other arcs with grey
-              // ?.style.fill = 'grey'
-          //   }
-          // })
-          // console.log(formattedData)
+        onMouseEnter={mouseOverHandler}
+        // onMouseMove={(_data, event) => {
+        //   console.log('enter pie')
+        //   let arc = event.target
 
+        // //  ( event.target !== arc ? mouseLeaveHandler() : null)
 
-          // event.target.style.fill = 'black';
-          // event.target.style.fill = '#9EE1CE';
-          // event.target.style.cursor = 'pointer'
-          // event.target.style.strokeWidth = '2'
-          event.target.style.opacity = '0.2'
-          }}
-        onMouseLeave={(_data, event) => {
-            // event.target.style.fill = "";
-            event.target.style.cursor = 'auto'
-            // event.currentTarget.style.strokeWidth = '' + borderWidth
-            event.target.style.opacity = '1'
-          }}
+        //   let newColors = [...colors].map(color => {
+        //     return color !== arc.getAttribute('fill') ?
+        //       color = 'lightgray'
+        //       : color
+        //   })
+
+        //   setColors(newColors)
+
+        //   // event.target.style.fill = 'black'
+        //   // event.target.style.fill = '#9EE1CE';
+        //   // event.target.style.cursor = 'pointer'
+        //   // event.target.style.strokeWidth = '2'
+        //   // event.target.style.opacity = '0.2'
+        // }}
+        onMouseLeave={mouseLeaveHandler}
+        // onMouseLeave={(_data, event) => {
+        //   setColors(designSystemColors)
+
+        //   // event.target.style.fill = "";
+        //   // event.target.style.cursor = 'auto'
+        //   // event.currentTarget.style.strokeWidth = '' + borderWidth
+        //   // event.target.style.opacity = '1'
+        // }}
       />
     </div>
   )
 }
 
-// PieChartNivo.propTypes = propTypes
-export default PieChart
+PieChart.propTypes = propTypes
--- src/stories/nivo.stories.js
+++ src/stories/nivo.stories.js
@@ -1,18 +1,18 @@
-import React from 'react';
-import PieChart from '../components/PieChart';
+import React from 'react'
+import PieChart from '../components/PieChart'
 // import { action } from '@storybook/addon-actions';
 
 export default {
   component: PieChart,
   title: 'Nivo',
-};
+}
 
 export const PieCharts = () => {
-return (
-<>
-    <div>Small</div>
-    <PieChart width={300}></PieChart>
-    <div>Medium</div>
-    <PieChart width={400}></PieChart>
-</>
-)}
+  return (
+    <>
+      <div>Small</div>
+      <PieChart width={300}></PieChart>
+      <div>Medium</div>
+      <PieChart width={400}></PieChart>
+    </>
+  )}
