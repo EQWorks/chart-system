b51fe32d905ad9a14a5a5d0c148b12f462338d3d
goondrious <srs_34@hotmail.com>
2020-06-16 11:25:44 -0400

    App - extract common storybook wrapper

--- src/stories/scatter.stories.js
+++ src/stories/scatter.stories.js
@@ -1,42 +1,18 @@
 import React from 'react'
+import { storiesOf } from '@storybook/react'
 
 import ScatterChart from '../components/scatter-chart'
 import scatterChartData from '../shared/data/scatter-chart-data'
+import ResponsiveChartWrapper from './responsive-chart-wrapper'
 
-import GridLayout, { WidthProvider } from 'react-grid-layout'
-
-import 'react-grid-layout/css/styles.css'
-import 'react-resizable/css/styles.css'
-
-import styled from 'styled-components'
-
-import { storiesOf } from '@storybook/react'
 
 export default {
   title: 'Scatter Chart'
 }
 
-const layout = [
-  { i: 'a', x: 0, y: 0, w: 6, h: 6 }
-]
-
-const Wrapper = styled.div`
-    width: ${ props => props.wrapperWidth}px;
-    height: ${ props => props.wrapperHeight}px;
-    border-style: solid;
-    border-width: 0.01px;
-    box-shadow: 0 4px 8px 0 rgba(0,0,0,0.2);
-    display: flex;
-    flex-direction: column;
-  `
-
-const ResponsiveGridLayout = WidthProvider(GridLayout)
-
 storiesOf('ScatterChart', module)
   .add('Widget Scatter Chart', () => (
-    <ResponsiveGridLayout className='layout' layout={layout} cols={300} rowHeight={30} >
-      <Wrapper key='a'>
-        <ScatterChart data={scatterChartData} axisBottomLegendLabel={'axisBottomLegend'} axisLeftLegendLabel={'axisLeftLegend'}></ScatterChart>
-      </Wrapper>
-    </ResponsiveGridLayout>
+    <ResponsiveChartWrapper>
+      <ScatterChart data={scatterChartData} axisBottomLegendLabel={'axisBottomLegend'} axisLeftLegendLabel={'axisLeftLegend'}></ScatterChart>
+    </ResponsiveChartWrapper>
   ))
