519d01a559ce91835a862173bc8366178fb2aeb9
goondrious <srs_34@hotmail.com>
2020-06-16 14:14:28 -0400

    Comp - add ChartWrapper for consistent styling and AutoSizer usage

---
+++ src/components/chart-wrapper/index.js
@@ -0,0 +1,57 @@
+import React from 'react'
+import PropTypes from 'prop-types'
+
+import styled from 'styled-components'
+
+import 'react-virtualized/styles.css'
+import { AutoSizer } from 'react-virtualized'
+
+
+const Title = styled.div`
+  margin: 16px 16px 0 16px;
+  height: 24px;
+  font-size: 18px;
+`
+
+const ChartContainer = styled.div`
+  display: flex;
+  flex: 1;
+  height: 100%;
+  margin: 0px 16px 16px 16px;
+`
+
+const ChartInner = styled.div`
+  position: relative;
+  width: ${ props => props.width}px;
+  height: ${ props => props.height}px;
+`
+
+const propTypes = {
+  title: PropTypes.string,
+  chart: PropTypes.element.isRequired,
+}
+
+const defaultProps = { title: '' }
+
+const ChartWrapper = ({
+  title,
+  chart,
+}) => (
+  <>
+    <Title>{title}</Title>
+    <ChartContainer>
+      <AutoSizer>
+        {({ height, width }) => (
+          <ChartInner height={height} width={width}>
+            {chart}
+          </ChartInner>
+        )}
+      </AutoSizer>
+    </ChartContainer>
+  </>
+)
+
+ChartWrapper.defaultProps = defaultProps
+ChartWrapper.propTypes = propTypes
+
+export default ChartWrapper
