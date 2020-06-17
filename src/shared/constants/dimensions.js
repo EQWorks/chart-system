cdd991b29fcb03296c0bcd32c46fd14b86c2d418
Erika Szabo <erika.szabo.geo@gmail.com>
2020-06-17 09:08:10 -0400

    Scatter-Chart/Legend: added new constants - added TRIMMED_LEGEND_WIDTH = 42 - added LEGEND_COLUMN_FIXED_ELEMENTS_WIDTH = 38

--- src/shared/constants/dimensions.js
+++ src/shared/constants/dimensions.js
@@ -4,4 +4,8 @@ export const WIDTH_BREAKPOINT_3 = 336
 export const HEIGHT_BREAKPOINT_1 = 206
 export const HEIGHT_BREAKPOINT_2 = 230
 export const HEIGHT_BREAKPOINT_3 = 310
-export const LEGEND_LABEL_WIDTH = 42
+// minimum width in pixels of the legend keys when we start trimming if necessary
+export const TRIMMED_LEGEND_WIDTH = 42
+// 12(space to chart to left) + 8(symbol-circle) + 6(space)+ legendLength + 12(space to margin) = 38
+// all spaces around and elements of the column legend, everything minus key length in pixels
+export const LEGEND_COLUMN_FIXED_ELEMENTS_WIDTH = 38
