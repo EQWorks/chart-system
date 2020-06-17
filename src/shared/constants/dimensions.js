7d9051c4e2b160421fd50689d294e53e4cc75218
Erika Szabo <erika.szabo.geo@gmail.com>
2020-06-17 17:59:05 -0400

    Scatter-Chart/Legend: edited comments

--- src/shared/constants/dimensions.js
+++ src/shared/constants/dimensions.js
@@ -6,6 +6,6 @@ export const HEIGHT_BREAKPOINT_2 = 230
 export const HEIGHT_BREAKPOINT_3 = 310
 // minimum width in pixels of the legend keys when we start trimming if necessary
 export const TRIMMED_LEGEND_WIDTH = 42
-// 12(space to chart to left) + 8(symbol-circle) + 6(space)+ legendLength + 12(space to margin) = 38
+// 38 = 12(space to chart to left) + 8(symbol-circle) + 6(space)+ legendLength + 12(space to margin)
 // all spaces around and elements of the column legend, everything minus key length in pixels
 export const LEGEND_COLUMN_FIXED_ELEMENTS_WIDTH = 38
