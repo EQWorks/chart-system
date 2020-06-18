1dadd97cd1a4a349655adad663e358bc06c43814
Erika Szabo <erika.szabo.geo@gmail.com>
2020-06-18 15:21:30 -0400

    Scatter-Chart/Legend-Portrait: added extra constants TEXT_HEIGHT - BUFFER - LEGEND_ROW_FIXED_ELEMENTS_WIDTH

--- src/shared/constants/dimensions.js
+++ src/shared/constants/dimensions.js
@@ -1,11 +1,17 @@
+export const WIDTH_BREAKPOINT_0 = 208
 export const WIDTH_BREAKPOINT_1 = 232
 export const WIDTH_BREAKPOINT_2 = 256
 export const WIDTH_BREAKPOINT_3 = 336
 export const HEIGHT_BREAKPOINT_1 = 206
 export const HEIGHT_BREAKPOINT_2 = 230
 export const HEIGHT_BREAKPOINT_3 = 310
+export const TEXT_HEIGHT = 17
+export const BUFFER = 8
 // minimum width in pixels of the legend keys when we start trimming if necessary
 export const TRIMMED_LEGEND_WIDTH = 42
-// 38 = 12(space to chart to left) + 8(symbol-circle) + 6(space)+ legendLength + 12(space to margin)
+// 38 = 12(space to chart to left) + 8(symbol-circle) + 6(space) + 12(space to margin)
 // all spaces around and elements of the column legend, everything minus key length in pixels
 export const LEGEND_COLUMN_FIXED_ELEMENTS_WIDTH = 38
+// 31 = 8(symbol-circle) + 6(buffer to key) + 17(margin right)
+// all spaces of an item of the row legend, everything minus key length in pixels
+export const LEGEND_ROW_FIXED_ELEMENTS_WIDTH = 31
