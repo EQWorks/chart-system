420ffd7d9615a66bda8ef85ba0dbb4bc97124295
Erika Szabo <erika.szabo.geo@gmail.com>
2020-07-03 09:42:44 -0400

    Pie-Chart/Update: added more data sets

--- src/shared/data/pie-chart-data.js
+++ src/shared/data/pie-chart-data.js
@@ -1,20 +1,50 @@
 //DATA FROM RANDOM EXAMPLE
 const pieChartData = [
   {
+    'id': 'css',
+    'label': 'css',
+    'value': 10,
+  },
+  {
     'id': 'erlang testing',
     'label': 'erlang',
-    'value': 461,
+    'value': 140,
+  },
+  {
+    'id': 'css1',
+    'label': 'css1',
+    'value': 10,
+  },
+  {
+    'id': 'erlang testing 1',
+    'label': 'erlang 1',
+    'value': 110,
+  },
+  {
+    'id': 'css2',
+    'label': 'css2',
+    'value': 70,
   },
   {
     'id': 'elixir salak',
     'label': 'elixir',
-    'value': 300,
+    'value': 60,
   },
   {
-    'id': 'css',
-    'label': 'css',
-    'value': 66,
-  }
+    'id': 'css3',
+    'label': 'css3',
+    'value': 10,
+  },
+  {
+    'id': 'elixir salak 1',
+    'label': 'elixir 1',
+    'value': 190,
+  },
+  {
+    'id': 'harry',
+    'label': 'harry',
+    'value': 21,
+  },
 ]
 
 export default pieChartData
