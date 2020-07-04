2fe78497ce45aec7d35761b2b47b9a8d4cb06429
Erika Szabo <erika.szabo.geo@gmail.com>
2020-07-03 20:07:58 -0400

    Pie-Chart/Update: deleted some data series

--- src/shared/data/pie-chart-data.js
+++ src/shared/data/pie-chart-data.js
@@ -16,35 +16,10 @@ const pieChartData = [
     'value': 10,
   },
   {
-    'id': 'erlang testing 1',
-    'label': 'erlang 1',
-    'value': 110,
-  },
-  {
-    'id': 'css2',
-    'label': 'css2',
-    'value': 70,
-  },
-  {
     'id': 'elixir salak',
     'label': 'elixir',
     'value': 60,
-  },
-  {
-    'id': 'css3',
-    'label': 'css3',
-    'value': 10,
-  },
-  {
-    'id': 'elixir salak 1',
-    'label': 'elixir 1',
-    'value': 190,
-  },
-  {
-    'id': 'harry',
-    'label': 'harry',
-    'value': 21,
-  },
+  }
 ]
 
 export default pieChartData
