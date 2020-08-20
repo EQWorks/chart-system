5fccf49e435e7f12f1101f2337a08da3bc92de58
Erika Szabo <erika.szabo.geo@gmail.com>
2020-08-20 09:08:24 -0400

    Wrapper: include in ChartWrapper component - chart-wrapper/index.js: included Wrapper styled component - responsive-chart-wrapper.js: replaced Wrapper with simpler ChartDiv styled component - CHANGELOG.md: added Wrapper changes

--- stories/responsive-chart-wrapper.js
+++ stories/responsive-chart-wrapper.js
@@ -3,19 +3,15 @@ import PropTypes from 'prop-types'
 import GridLayout, { WidthProvider } from 'react-grid-layout'
 import 'react-grid-layout/css/styles.css'
 import 'react-resizable/css/styles.css'
-
 import styled from 'styled-components'
 
+const ResponsiveGridLayout = WidthProvider(GridLayout)
 
-const Wrapper = styled.div`
+const ChartDiv = styled.div`
   border-style: solid;
   border-width: 0.01px;
   box-shadow: 0 4px 8px 0 rgba(0,0,0,0.2);
-  display: flex;
-  flex-direction: column;
 `
-
-const ResponsiveGridLayout = WidthProvider(GridLayout)
 
 const layout = [
   { i: 'a', x: 0, y: 0, w: 200, h: 6 }
@@ -25,9 +21,9 @@ const propTypes = { children: PropTypes.element }
 
 const ResponsiveChartWrapper = ({ children }) => (
   <ResponsiveGridLayout className='layout' layout={layout} cols={300} rowHeight={30}>
-    <Wrapper key='a'>
+    <ChartDiv key='a' >
       {children}
-    </Wrapper>
+    </ChartDiv>
   </ResponsiveGridLayout>
 )
 
