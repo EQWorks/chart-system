a0e3439e1a9c8d27a6b5e34fb2e6128e9f46375a
DoParkEQ <do.park@eqworks.com>
2021-04-28 10:34:42 -0400

    d3/singlebar - add animation to svg elements

--- src/components/visx/single-bar.js
+++ src/components/visx/single-bar.js
@@ -31,18 +31,21 @@ const SingleBar = ({ data, width, height, config }) => {
 
   const path = d3.line()([[0, 0], [width, 0]])
 
+  const filledAmt = xScale(percentage)
 
   return (
     <svg width={width + padding * 1.5} height={height + padding}>
       <g transform={`translate(${padding / 2},${padding / 2})`}>
         <rect width={width} height={rectHeight} style={{ fill: '#cdcdcd' }}></rect>
-        <rect width={xScale(percentage)} height={rectHeight} style={{ fill: '#0075FF' }}></rect>
+        <rect width={filledAmt} height={rectHeight} style={{ fill: '#0075FF' }}>
+          <animate attributeName='width' values={`0; ${filledAmt * 0.2}; ${filledAmt};`} keytimes='0.0;0.5;1.0;' dur='0.3s' repeatCount='1' />
+        </rect>
         <g transform={`translate(0 ${rectHeight + spacing})`}>
           {/* <path d={path} stroke='black' /> */}
           {ticks.map(tick =>
             <g>
               <line x1={xScale(tick)} y1={0} x2={xScale(tick)} y2={8} stroke='black' />
-              <text style={{ textAnchor: 'middle' }} transform={`translate(${xScale(tick)}, ${25})`}>{`${tick * 100}%`}</text>
+              <text style={{ textAnchor: 'middle', fontFamily: 'Open Sans', fontSize: '12px' }} transform={`translate(${xScale(tick)}, ${25})`}>{`${tick * 100}%`}</text>
             </g>
           )}
         </g>
