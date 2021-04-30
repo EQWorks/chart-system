86bc26d6451ffe40bd258dee4ace81c5c3dc1dc6
DoParkEQ <do.park@eqworks.com>
2021-04-30 14:21:16 -0400

    single bar - add responsiveness + pop structure modification

--- src/components/d3/single-bar.js
+++ src/components/d3/single-bar.js
@@ -2,34 +2,34 @@ import PropTypes from 'prop-types'
 import React from 'react'
 import * as d3 from 'd3'
 
+// const percentage = 0.25;
 
-const percentage = 0.25;
-const rectHeight = 100;
-const padding = 50;
+const padding = 20;
 const spacing = 10;
+
 const SingleBar = ({ data, width, height, config }) => {
 
-  const { dataKey, color } = config;
-
+  const { dataKey, color, backgroundColor } = config;
+  const percentage = data[dataKey.x1] / data[dataKey.x2];
   const xScale = d3.scaleLinear()
     .domain([0, 1])
     .range([0, width]);
 
   const ticks = xScale.nice().ticks(10); //ticks are calculated based on domain range
-
   const path = d3.line()([[0, 0], [width, 0]])
-
   const filledAmt = xScale(percentage)
-
+  console.log(width)
+  const adjustment = padding * 2
+  const rectHeight = height * 0.7;
+  console.log(filledAmt)
   return (
-    <svg width={width + padding * 1.5} height={height + padding}>
-      <g transform={`translate(${padding / 2},${padding / 2})`}>
-        <rect width={width} height={rectHeight} style={{ fill: '#cdcdcd' }}></rect>
-        <rect width={filledAmt} height={rectHeight} style={{ fill: '#0075FF' }}>
-          <animate attributeName='width' values={`0; ${filledAmt * 0.2}; ${filledAmt};`} keytimes='0.0;0.5;1.0;' dur='0.3s' repeatCount='1' />
+    <svg width={width} height={height}>
+      <g transform={`translate(${adjustment / 2},${adjustment / 2})`}>
+        <rect rx={8} width={width - adjustment} height={rectHeight} style={{ fill: backgroundColor }}></rect>
+        <rect rx={8} width={filledAmt} height={rectHeight} style={{ fill: color }}>
+          <animate attributeName='width' from='0' to={filledAmt} dur='0.3s' repeatCount='1' />
         </rect>
         <g transform={`translate(0 ${rectHeight + spacing})`}>
-          {/* <path d={path} stroke='black' /> */}
           {ticks.map(tick =>
             <g>
               <line x1={xScale(tick)} y1={0} x2={xScale(tick)} y2={8} stroke='black' />
--- stories/single-bar.stories.js
+++ stories/single-bar.stories.js
@@ -1,21 +1,39 @@
 import React from 'react'
 import SingleBar from '../src/components/d3/single-bar'
 import { geoCohortData } from './data/atom/geo-cohort'
+import { ParentSize } from '@visx/responsive'
 export default {
   title: 'D3/bar',
 }
 
 const [IDdata] = geoCohortData.aggregated.GeoCohortListID
 console.log(IDdata)
+
 const barConfig = {
   color: '#0075FF',
+  backgroundColor: '#cdcdcd',
   dataKey: {
-    x: ['Imps', 'Bids'],
-    //y: 'Age',
+    x1: 'Imps',
+    x2: 'Bids',
   },
 }
 
-export const Default = () => <SingleBar data={IDdata} width={500} height={500} config={barConfig} />
+export const Default = () => {
+  const [width, setWidth] = React.useState(500)
+  // React.useEffect(() => {
+  //   const interval = setInterval(() => setWidth(Math.random() * 200 + 300), 2000);
+  //   return () => {
+  //     clearInterval(interval)
+  //   }
+  // }, [])
 
+  return <div style={{ width: width, height: 200, border: '1px solid black' }}>
+    <ParentSize>
+      {parent => (
+        <SingleBar data={IDdata} width={width} height={parent.height} config={barConfig} />
+      )}
+    </ParentSize>
+  </div>
+}
 //win rate = imps/bids
 //CTR = clicks/impressions
