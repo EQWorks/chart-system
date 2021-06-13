30cdd944593d5ed090e3832bb7c27a73265be9d8
Erika Szabo <erika.szabo.geo@gmail.com>
2021-06-12 21:54:21 -0400

    Stories - add color as a function stories for all charts

---
+++ stories/color.stories.js
@@ -0,0 +1,91 @@
+import React from 'react'
+import { storiesOf } from '@storybook/react'
+
+import BarChart from '../src/components/bar-chart/'
+import PieChart from '../src/components/pie-chart'
+import LineChart from '../src/components/line-chart'
+import ScatterChart from '../src/components/scatter-chart'
+import barChartData from './data/bar-chart-data'
+import lineChartData from './data/line-chart-data'
+import pieChartData from './data/pie-chart-data'
+import scatterChartData from './data/scatter-chart-data'
+import ResponsiveChartWrapper from './responsive-chart-wrapper'
+
+
+const title = 'My Title'
+const palette = ['#F936A9', '#B54DD5', '#FF4774', '#FFA600', '#1564ec', '#FF7641', '#FFA600']
+
+const colorsMap = {}
+
+storiesOf('Color', module)
+  .add('Bar Chart with Color function', () => {
+    const barIds = ['visits', 'visitors', 'single_visitors', 'repeat_visitors', 'repeat_visitor_rate', 'multi_visitors', 'multi_visitor_rate']
+    barIds.forEach((d, i) => {
+      colorsMap[d] = palette[i]
+    })
+    return (
+      <ResponsiveChartWrapper>
+        <BarChart
+          title={ title }
+          data={ barChartData }
+          axisBottomLegendLabel='Address City'
+          axisLeftLegendLabel='Visitors'
+          groupMode='grouped'
+          colors={d => colorsMap[d]}
+        />
+      </ResponsiveChartWrapper>
+    )
+  })
+  .add('Pie Chart with Color function', () => { 
+    const pieIds = ['css', 'erlang testing', 'css1', 'elixir salak']
+    pieIds.forEach((d, i) => {
+      colorsMap[d] = palette[i]
+    })  
+    return (
+      <ResponsiveChartWrapper>
+        <PieChart
+          title={title}
+          data={pieChartData}
+          isDonut={false}
+          colors={d => colorsMap[d.id]}
+        />
+      </ResponsiveChartWrapper>
+    )
+  })
+  .add('Line Chart with Color function', () => {
+    const LineIds = ['france', 'us', 'germany', 'norway']
+    LineIds.forEach((d, i) => {
+      colorsMap[d] = palette[i]
+    })  
+    return (
+      <ResponsiveChartWrapper>
+        <LineChart
+          title={ title }
+          data={ lineChartData }
+          indexBy='country'
+          xKey='vehicle'
+          yKeys={ ['amount'] }
+          xScale={{ type: 'point' }}
+          axisBottomLegendLabel={ 'axisBottomLegend' }
+          axisLeftLegendLabel={ 'axisLeftLegend' }
+          colors={d => colorsMap[d.id]}
+        />
+      </ResponsiveChartWrapper>
+    )
+  })
+  .add('Scatter Chart with Color function', () => {
+    const scatterIds = ['Arts & Entertainment', 'Science - Technology', 'Hobbies, (Vacation)']
+    scatterIds.forEach((d, i) => {
+      colorsMap[d] = palette[i]})
+    return (
+      <ResponsiveChartWrapper>
+        <ScatterChart
+          title={title}
+          data={scatterChartData}
+          axisBottomLegendLabel={'axisBottomLegend'}
+          axisLeftLegendLabel={'axisLeftLegend'}
+          colors={d => colorsMap[d.id]}
+        />
+      </ResponsiveChartWrapper>
+    )
+  })
