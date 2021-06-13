27136d6d554c446b4e0a74eedb07fd84fbc22e76
Erika Szabo <erika.szabo.geo@gmail.com>
2021-06-12 21:54:21 -0400

    Stories - add color as a function stories for all charts

--- stories/scatter.stories.js
+++ stories/scatter.stories.js
@@ -7,29 +7,15 @@ import ResponsiveChartWrapper from './responsive-chart-wrapper'
 
 
 const title = 'My Title'
-const palette = ['#1564ec', '#FF7641', '#FFA600', '#F936A9', '#B54DD5', '#FF4774']
-const colorsMap = {}
-scatterChartData.forEach((d, i) => colorsMap[d.Name] = palette[i])
 
 storiesOf('ScatterChart', module)
-  .add('Widget Scatter Chart', () => (
+  .add('Scatter Chart', () => (
     <ResponsiveChartWrapper>
       <ScatterChart
         title={ title }
         data={ scatterChartData }
         axisBottomLegendLabel={ 'axisBottomLegend' }
         axisLeftLegendLabel={ 'axisLeftLegend' }
-      />
-    </ResponsiveChartWrapper>
-  ))
-  .add('Widget Scatter Chart with colors as function', () => (
-    <ResponsiveChartWrapper>
-      <ScatterChart
-        title={ title }
-        data={ scatterChartData }
-        axisBottomLegendLabel={ 'axisBottomLegend' }
-        axisLeftLegendLabel={ 'axisLeftLegend' }
-        colors={(d) => colorsMap[d.serieId]}
       />
     </ResponsiveChartWrapper>
   ))
