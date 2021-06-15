80f8ee6b8cd5742fdb357cf60a7897220ffdd77a
DoParkEQ <do.park@eqworks.com>
2021-06-15 12:41:31 -0400

    d3/gaugeArc - add animation controller

--- src/components/d3/gauge-arc.js
+++ src/components/d3/gauge-arc.js
@@ -1,44 +1,16 @@
 import PropTypes from 'prop-types'
-import React, { useRef, useEffect } from 'react'
+import React, { useRef, useEffect, useMemo } from 'react'
 import * as d3 from 'd3'
 
 const GaugeArc = ({ data, width, height, config }) => {
 
-  const { dataKey, color, backgroundColor } = config
+  const { color, backgroundColor, dataKey, transition } = config
 
-  const value = data[dataKey.x1] / data[dataKey.x2]
-  const remainder = 1.0 - value
+  const value = data[dataKey.x2] ? data[dataKey.x1] / data[dataKey.x2] * 100 : 0
+
+  const remainder = 100 - value
   const colors = [color, backgroundColor]
   const svgRef = useRef(null)
-
-  useEffect(() => {
-    svgRef.current && d3.select(svgRef.current)
-      .selectAll('.arcs')
-      .data(arcsData)
-      .attr('d', arcPath)
-      .attr('fill', (_, i) => colors[i])
-      .transition()
-      .duration(1000)
-      .attrTween('d', function (d) {
-        if (d.index === 1) {
-          return //deactivate tweening for the remainder value
-        }
-        var i = d3.interpolate(d.startAngle + 0.1, d.endAngle)
-        return function (t) {
-
-          d.endAngle = i(t)
-          return arcPath(d)
-        }
-      })
-  }, [data])
-
-  // when a > b
-  // sort = b - a
-  // flipped = false
-
-  //when a < b
-  //sort = a - b
-  //flipped = false
 
   const isAscending = value > remainder
 
@@ -48,11 +20,40 @@ const GaugeArc = ({ data, width, height, config }) => {
 
   const arcsGenerator = d3.pie().sortValues(function (a, b) { return isAscending ? b - a : a - b })
 
-  const arcsData = arcsGenerator([value, remainder])
+  const arcsData = useMemo(() => arcsGenerator([value, remainder]), [data])
 
   const arcPath = d3.arc().innerRadius(pieSize - lineThickness).outerRadius(pieSize).cornerRadius(lineThickness)
 
   const [backgroundPath] = d3.pie()([1.0])
+
+  useEffect(() => {
+    if (svgRef.current !== null) {
+      const svg = d3.select(svgRef.current)
+
+      const gauge = svg
+        .selectAll('.arcs')
+        .data(arcsData)
+        .attr('d', arcPath)
+        .attr('fill', (_, i) => colors[i])
+
+      if (transition.animation) {
+        gauge
+          .transition()
+          .duration(transition.duration)
+          .attrTween('d', function (d) {
+            if (d.index === 1) {
+              return //deactivate tweening for the remainder value
+            }
+            var i = d3.interpolate(d.startAngle + 0.1, d.endAngle)
+            return function (t) {
+
+              d.endAngle = i(t)
+              return arcPath(d)
+            }
+          })
+      }
+    }
+  }, [svgRef.current, arcsData])
 
   return (
     <svg preserveAspectRatio="xMidYMid meet" ref={svgRef} width={width} height={height}>
@@ -65,19 +66,25 @@ const GaugeArc = ({ data, width, height, config }) => {
           fontFamily: 'Open Sans',
           fontSize: '24px',
           fontWeight: 700,
-        }}>{`${value.toFixed(4) * 100}%`}</text>
+        }}>{`${value.toFixed(2)}%`}</text>
       </g>
     </svg >
   )
 }
 
 GaugeArc.propTypes = {
+  backgroundColor: PropTypes.string,
+  color: PropTypes.string,
   config: PropTypes.shape({
     backgroundColor: PropTypes.string,
     color: PropTypes.string,
     dataKey: PropTypes.shape({
       x1: PropTypes.string,
       x2: PropTypes.string,
+    }),
+    transition: PropTypes.shape({
+      animation: PropTypes.any,
+      duration: PropTypes.any,
     }),
   }).isRequired,
   data: PropTypes.object.isRequired,
@@ -88,7 +95,12 @@ GaugeArc.propTypes = {
 GaugeArc.defaultProps = {
   color: '#0075FF',
   backgroundColor: '#cdcdcd',
+  transition: {
+    animation: false,
+    duration: 0,
+  },
 }
 
 
-export default GaugeArc
+export default React.memo(GaugeArc)
+
