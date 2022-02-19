7c6060ff1efce17eca9fe4c55ad547e10a2a9932
Kyle Grimsrud-Manz <hi@kylegrimsrudma.nz>
2022-02-18 21:04:08 -0500

    plotly - allow axis titles in line, scatter, bar

--- src/components/plotly/bar/index.js
+++ src/components/plotly/bar/index.js
@@ -10,6 +10,9 @@ const Bar = ({
   data,
   stacked,
   showTicks,
+  showAxisTitles,
+  x,
+  y,
   ...props
 }) => (
   <CustomPlot
@@ -18,6 +21,8 @@ const Bar = ({
       useTransformedData({
         type: 'bar',
         data,
+        x,
+        y,
         ...props,
       })
     }
@@ -26,10 +31,23 @@ const Bar = ({
       xaxis: {
         showticklabels: showTicks,
         automargin: true,
+        type: 'category',
+        ...(showAxisTitles && {
+          title: {
+            text: x,
+            standoff: 20,
+          },
+        }),
       },
       yaxis: {
         showticklabels: showTicks,
         automargin: true,
+        ...(showAxisTitles && y?.length === 1 && {
+          title: {
+            text: y[0],
+            standoff: 30,
+          },
+        }),
       },
     }}
     {...props}
@@ -41,12 +59,14 @@ Bar.propTypes = {
   y: PropTypes.arrayOf(PropTypes.string).isRequired,
   stacked: PropTypes.bool,
   showTicks: PropTypes.bool,
+  showAxisTitles: PropTypes.bool,
   ...plotlyPropTypes,
 }
 
 Bar.defaultProps = {
   stacked: false,
   showTicks: true,
+  showAxisTitles: true,
   ...plotlyDefaultProps,
 }
 
